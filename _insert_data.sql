a4f584d291d81b180b223cf07bde0f9394dc6e38
ValeskaCardoso01 <valeskacardoso01@gmail.com>
2025-12-01 22:07:48 -0300

    Create _insert_data.sql

---
+++ _insert_data.sql
@@ -0,0 +1,7 @@
+-- Inserindo alunos
+INSERT INTO Aluno (id_aluno, nome, CPF, telefone, email, data_nascimento, status)
+VALUES
+(1, 'João Silva', '12345678900', '11988887777', 'joao@gmail.com', '1995-10-20', 'Ativo'),
+(2, 'Maria Souza', '98765432100', '11999996666', 'maria@gmail.com', '1998-05-12', 'Ativo'),
+(3, 'Pedro Rocha', '45678912311', '11973335544', 'pedro@gmail.com', '1997-07-07', 'Inativo');
+...
